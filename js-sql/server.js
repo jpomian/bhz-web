1e8567dee9c7072cfd4ba88686271a00314ce40d
krrdash <jpomianowski@outlook.com>
2025-08-17 16:57:30 +0200

    Updated query

--- js-sql/server.js
+++ js-sql/server.js
@@ -6,6 +6,21 @@ require('dotenv').config()
 const app = express();
 app.use(cors());
 
+const sql = `SELECT
+    sql_mainstats.Nick,
+    sql_mainstats.Infections,
+    sql_mainstats.Kills,
+    sql_times.\`Time Played\` AS \`Time\`,
+    sql_times.\`Last Seen\` AS \`Last\`
+FROM
+    sql_mainstats
+INNER JOIN
+    sql_times
+ON
+    sql_mainstats.Auth = sql_times.Auth
+  ORDER BY
+    sql_mainstats.Kills DESC`;
+
 const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -17,7 +32,7 @@ const dbConfig = {
 app.get('/api/players', async (req, res) => {
   try {
     const connection = await mysql.createConnection(dbConfig);
-    const [rows] = await connection.query('SELECT * FROM players_stats');
+    const [rows] = await connection.query(sql);
     connection.end();
     res.json(rows);
   } catch (error) {
